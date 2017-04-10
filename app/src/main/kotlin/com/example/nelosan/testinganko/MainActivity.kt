b11ac609eb3fefb1ef51dcc42b24f9a1337f0399
nelosan <nelosan@gmail.com>
2017-04-10 18:15:57 +0200

    Add Kotlin and Ando

---
+++ app/src/main/kotlin/com/example/nelosan/testinganko/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.nelosan.testinganko
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by manuelsanchez on 10/4/17.
+ */
+class MainActivity : AppCompatActivity() {
+}
